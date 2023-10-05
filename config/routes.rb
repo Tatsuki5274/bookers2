f5ff42adcfb7009c95f272263dea2be2649f699b
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-05 20:19:53 +0900

    未ログイン時のメニュー実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # get 'books/new'
   # get 'books/edit'
   root to: 'homes#top'
+  get "/about" => "homes#about", as: "about"
   devise_for :users
   resources :users, only: [:index, :edit, :show]
   resources :books, only: [:index, :edit, :show, :update, :delete]

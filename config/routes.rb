81f6978bfe11bd1b6adba84e9d90ad64c213eb05
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-05 20:15:54 +0900

    ヘッダーのリンク作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # get 'users/edit'
+  # get 'books/index'
+  # get 'books/new'
+  # get 'books/edit'
   root to: 'homes#top'
   devise_for :users
+  resources :users, only: [:index, :edit, :show]
+  resources :books, only: [:index, :edit, :show, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

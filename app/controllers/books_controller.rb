0a11ca063bbcf8ec140925cd64cf95753559bacc
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-06 20:57:32 +0900

    bookの変更を実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,6 +13,7 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -24,6 +25,15 @@ class BooksController < ApplicationController
       flash[:errors] = book.errors
       redirect_to books_path()
     end
+  end
+
+  def update
+      @book = Book.find(params[:id])
+      if @book.update(book_params)
+        redirect_to book_path(@book.id)
+      else
+        render "edit"
+      end
   end
 
   def destroy

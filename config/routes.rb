9ec260256847f5fab257292601aac3b3a61099db
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-06 17:27:44 +0900

    プロフィール変更機能を実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get "/about" => "homes#about", as: "about"
   devise_for :users
-  resources :users, only: [:index, :edit, :show]
+  resources :users, only: [:index, :edit, :show, :update]
   resources :books, only: [:index, :edit, :show, :create, :update, :delete]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

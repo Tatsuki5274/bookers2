bc63b52629d338fadf138886c65e3129b40895d1
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-06 16:53:17 +0900

    usersの一覧を作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all()
   end
 
   def show
-    @books = Book.where(user_id: current_user.id)
+    @books = Book.where(user_id: params[:id])
   end
 
   def edit
